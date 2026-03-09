995977e750356af015b01cd67cc1d0830efb9cd9
IDOWUMAYOWA <sangotadeidowu@yahoo.com>
2026-03-09 02:05:42 +0100

    Base model added

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from CNNVision import logger
 from CNNVision.pipeline.stage_1_data_ingestion import DataIngestionTraining
+from CNNVision.pipeline.stage_2_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTraining()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
