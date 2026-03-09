1d1937811e3ae76af48793b25c50e48b94c6c939
IDOWUMAYOWA <sangotadeidowu@yahoo.com>
2026-03-09 01:21:48 +0100

    Ignore data_ingestion artifacts

---
+++ main.py
@@ -0,0 +1,12 @@
+from CNNVision import logger
+from CNNVision.pipeline.stage_1_data_ingestion import DataIngestionTraining
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTraining()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
